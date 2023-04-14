ab358a84c648f1fba7b2c201b3bfc678a2026fc5
Yunocat02 <p50248.48@gmail.com>
2023-04-14 14:35:51 +0700

    Finish Version.3

--- keptang_backend/db/keptang.sql
+++ keptang_backend/db/keptang.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 13, 2023 at 11:19 AM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.2.0
+-- Generation Time: Apr 14, 2023 at 09:30 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,28 +29,24 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `moneylist` (
   `list_id` int(11) NOT NULL,
-  `description` text NOT NULL,
+  `description` varchar(50) NOT NULL,
   `date` date NOT NULL,
   `amount` int(11) NOT NULL,
-  `types` text NOT NULL,
+  `types` varchar(10) NOT NULL,
   `user_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `moneylist`
 --
 
 INSERT INTO `moneylist` (`list_id`, `description`, `date`, `amount`, `types`, `user_id`) VALUES
-(1, 'พ่อให้', '2023-04-12', 12000, 'income', 40955),
-(3, 'สุ่มกาชา', '2023-04-12', 1000, 'expense', 40955),
-(4, 'ขายโลมา', '2023-04-12', 100, 'income', 40956),
-(5, 'ขายโลมา', '2023-04-12', 100, 'income', 40956),
-(6, 'ขายโลมา', '2023-04-14', 2000, 'income', 40954),
-(7, 'ทดสอบ', '2023-04-13', 1000, 'income', 40957),
-(8, 'ขายโลมา', '2023-04-12', 100, 'income', 40956),
-(9, 'รายรับทดสอบ', '2023-04-15', 1000, 'income', 40957),
-(10, 'รายรับทดสอบ', '2023-04-13', 800, 'income', 40954),
-(11, 'รายรับทดสอบ', '2023-04-13', 999, 'income', 40954);
+(1, 'รุทให้', '2023-04-13', 120, 'income', 40954),
+(39, 'พ่อให้', '2023-04-12', 500, 'income', 40956),
+(40, 'กินข้าว', '2023-04-13', 50, 'expense', 40956),
+(41, 'ขโมยเงิน', '2023-04-12', 5, 'income', 40956),
+(42, 'ฟองขอยืม', '2023-04-14', 20, 'expense', 40956),
+(43, 'แม่ให้', '2023-04-14', 100, 'income', 40956);
 
 -- --------------------------------------------------------
 
@@ -60,19 +56,17 @@ INSERT INTO `moneylist` (`list_id`, `description`, `date`, `amount`, `types`, `u
 
 CREATE TABLE `userdata` (
   `user_id` int(11) NOT NULL,
-  `user_name` text NOT NULL,
+  `user_name` varchar(50) NOT NULL,
   `balance_total` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `userdata`
 --
 
 INSERT INTO `userdata` (`user_id`, `user_name`, `balance_total`) VALUES
-(40954, 'chawanwit', 0),
-(40955, 'nirut', 11000),
-(40956, 'vivat', 15000),
-(40957, 'sedthanan', 0);
+(40954, 'chawanwit', 120),
+(40956, 'vivat', 535);
 
 --
 -- Indexes for dumped tables
@@ -98,13 +92,7 @@ ALTER TABLE `userdata`
 -- AUTO_INCREMENT for table `moneylist`
 --
 ALTER TABLE `moneylist`
-  MODIFY `list_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
-
---
--- AUTO_INCREMENT for table `userdata`
---
-ALTER TABLE `userdata`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40958;
+  MODIFY `list_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
